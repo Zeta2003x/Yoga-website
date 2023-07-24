e32a4aafb1b3f22665515073ec95ae1c8d0128c9
Matias Grin <mgrin@frba.utn.edu.ar>
2023-07-24 14:23:34 -0300

    Agrego signup

--- js/script.js
+++ js/script.js
@@ -40,6 +40,6 @@ videos.forEach((video) => {
 // Select the button
 const btn = document.querySelector(".btn-toggle");
 // Listen for a click on the button 
-btn.addEventListener("click", function() {
-    document.body.classList.toggle("dark-mode");
+btn.addEventListener("click", function () {
+  document.body.classList.toggle("dark-mode");
 });
